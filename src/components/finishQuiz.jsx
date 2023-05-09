4cc82297d34fd949ae14d49e9acc01ff7af159a1
Kofi Gyan <kgyan19lf@outlook.com>
2023-05-09 08:33:28 +0100

    alpha 1.1 total questions included in score

--- src/components/finishQuiz.jsx
+++ src/components/finishQuiz.jsx
@@ -1,12 +1,13 @@
 import {useContext} from 'react';
 import { QuizContext } from './quizContest';
+import {Quetions} from "./questionList";
 
 export const FinishQuiz=()=>{
   const {setAppState,score,attempts}=useContext(QuizContext);
 
 return(
   <div className="finish inColumn">
-    <h2>{attempts}{attempts===1 ?"st" :attempts===2?"nd":attempts===3?"rd" :"th"} attempt You scored {score}</h2>
+    <h2>{attempts}{attempts===1 ?"st" :attempts===2?"nd":attempts===3?"rd" :"th"} attempt You scored {score} / {Quetions.length} </h2>
     <button onClick={()=>{setAppState("menu")}}>Restart</button>
   </div>  
 );
